48293c6fe9ab03bdeb33ba9de4f89add68b78c02
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2026-02-05 21:36:26 +0200

    add LoginFragment and login screen layout

---
+++ app/src/main/java/com/tasneem/mealplanner/presentation/login/view/LoginFragment.java
@@ -0,0 +1,18 @@
+package com.tasneem.mealplanner.presentation.login.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.tasneem.mealplanner.R;
+
+public class LoginFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
