0367ae77e2bcc425ac09e8f1800e0459e7269db1
Tasneem AbdElhakeem <tasniim.hakiim@gmail.com>
2026-02-08 10:19:56 +0200

    implement navigation with BottomNavigationView and NavHostFragment

--- app/src/main/java/com/tasneem/mealplanner/MainActivity.java
+++ app/src/main/java/com/tasneem/mealplanner/MainActivity.java
@@ -7,6 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.navHostFragment);
+        NavController navController = Objects.requireNonNull(navHostFragment).getNavController();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
---
+++ app/src/main/java/com/tasneem/mealplanner/presentation/mealdetails/view/MealDetailsFragment.java
@@ -0,0 +1,18 @@
+package com.tasneem.mealplanner.presentation.mealdetails.view;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.tasneem.mealplanner.R;
+
+public class MealDetailsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_meal_details, container, false);
+    }
+}
